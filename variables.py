805757728d662ec8fad0e318fd79051ed27b9730
Poblete999 <Poblete999@users.noreply.github.com>
2023-04-12 15:06:04 -0400

    este es mi primer mensaje

---
+++ variables.py
@@ -0,0 +1,11 @@
+#ESTE ES UN COMENTARIO DE UNA LINEA
+
+nombre = "Benjamin"
+print("Hola mi nombre es",nombre)
+edad = "18"
+print("Mi nombre es", nombre)
+print("Mi edad es de", edad,"años")
+
+nombre1= input("¿Cual es tu nombre?")
+edad1 =
+print("Tu nombre es:", nombre1)
