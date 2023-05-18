2282bd7a50ff9ae4cdc0f45655fbc8f6e883035f
Poblete999 <129112124+Poblete999@users.noreply.github.com>
2023-05-17 21:38:51 -0400

    Delete RETOS.py

--- RETOS.py
+++
@@ -1,8 +0,0 @@
-
-#Ingrese por consola.
-
-nombre = input("¿Cual es tu nombre?")
-edad = input("¿Cual es tu edad?")
-print(type(edad))
-total = int(edad) + 20
-print("Hola mi nombre es", nombre, ",tengo", edad, "años y en 20 años tendré", total, "años") 
